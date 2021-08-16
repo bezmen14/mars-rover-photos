765961e472ffa91eb2ebd5f122822aabf7c4010d
Egor Bezmen <egor1911@gmail.com>
2021-08-16 19:55:59 +0300

    fixed

--- src/db/models/camera.js
+++ src/db/models/camera.js
@@ -4,13 +4,7 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
   class Camera extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate({ Rover }) {
-      // одна камера относится к нескольким марсоходам
       this.belongsTo(Rover, { foreignKey: 'roverid' });
     }
   }
--- src/db/models/rover.js
+++ src/db/models/rover.js
@@ -4,13 +4,7 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
   class Rover extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate({ Camera }) {
-      // у одного марсохода может быть много камер
       this.hasMany(Camera, { foreignKey: 'roverid' });
     }
   }
